1dc91f5d7aa6de2464198b6535eba1dccaff4723
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-06-21 00:52:36 -0400

    Update apt_dependencies.sh

--- dependency_scripts/apt_dependencies.sh
+++ dependency_scripts/apt_dependencies.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sudo apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm chromium pulseaudio alsa-utils pavucontrol rxvt-unicode -y
+# could also swap rxvt-unicode for terminator
