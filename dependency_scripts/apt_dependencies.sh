73900c7396281c366730e94fa597551480b8835f
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-06-29 18:08:30 -0400

    Update apt_dependencies.sh

--- dependency_scripts/apt_dependencies.sh
+++ dependency_scripts/apt_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt update -y
-sudo apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm chromium pulseaudio alsa-utils pavucontrol alacritty picom nvim -y
+sudo apt install i3 xorg lightdm slick-greeter lightdm-settings lxappearance nitrogen pcmanfm chromium pulseaudio alsa-utils pavucontrol alacritty picom neovim -y
 # could also swap rxvt-unicode for terminator
